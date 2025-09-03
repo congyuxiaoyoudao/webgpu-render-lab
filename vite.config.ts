07df71acb7ddbdb9de9b7dbe6189bc06e392db76
congyuxiaoyoudao <3187497704@qq.com>
2025-09-03 23:25:40 +0800

    Modify vite.config.ts to support multi-entry packing

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/webgpu-render-lab/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        helloTriangle: 'src/helloTriangle/index.html',
+        threeDGS: 'src/3dgs/index.html',
+      }
+    }
+  }
 })
